3516757a49b74cfaf6a1642d44991db99e491e02
daniel-u-c <dcast162@ucr.edu>
2021-11-14 22:22:21 -0800

    Fixes #42

---
+++ header/Bundle.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Content.hpp"
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+class Bundle : public Content {
+    private:
+        std::unordered_map<int, Content*> contentList;
+    public:
+        Bundle(std::string title, int ISBN, const std::vector<Content*>& contentList = {});
+        ~Bundle();
+
+        void Display();
+};
