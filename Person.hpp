b0a0041c34cdd7ac6da950b1d6dadbf048989fbd
davidry777 <dryan0319@gmail.com>
2021-11-15 18:03:18 -0800

    Fixes #47

--- Person.hpp
+++ Person.hpp
@@ -2,7 +2,7 @@
 #define __PERSON_HPP__
 
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -14,12 +14,13 @@ class Person
         int hashedPassword;
         LibrarySystem* library;
         
-        string GetName();
-        int GetId();
-        string CheckPerson(Person*);
-        string GetInfo();
-        int GetHashedPassword();
-        string GetBookInfo();
+        virtual ~Person() = default;
+        virtual std::string GetName() = 0;
+        virtual int GetId() = 0;
+        virtual std::string CheckPerson(Person* person) = 0;
+        virtual std::string GetInfo() = 0;
+        virtual int GetHashedPassword() = 0;
+        virtual std::string GetBookInfo() = 0;
 };
 
 #endif //__PERSON_HPP__
