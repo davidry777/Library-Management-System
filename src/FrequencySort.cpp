96bf43a8b6bccc6a08f5fa676632a9c96faccae0
jsongi <unavailablenamenow@gmail.com>
2021-11-15 16:08:01 -0800

    Implementation for frequency sort

---
+++ src/FrequencySort.cpp
@@ -0,0 +1,17 @@
+#include "../header/BookSort.h"
+
+class FrequencySort : public BookSort
+{
+	private:
+	
+	public:
+		std::vector<Book*>& SortBooks(std::vector<Book*>& catalogue)
+		{
+			//This uses the standard library sorting function to sort books alphabetically
+			std::sort(catalogue.begin(), catalogue.end(), this->compareFreq);
+			return catalogue;
+		}
+		static bool compareFreq(Book* a, Book* b)
+		{
+			return a->GetFrequency() > b->GetFrequency();
+		}
