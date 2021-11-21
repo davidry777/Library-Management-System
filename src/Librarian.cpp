692e51c696867ca2152fc3dd0a90e2ffa7c7cea7
davidry777 <dryan0319@gmail.com>
2021-11-20 17:06:43 -0800

    Fixed #26, Completed fixed #49

--- src/Librarian.cpp
+++ src/Librarian.cpp
@@ -10,3 +10,5 @@ Librarian::Librarian(std::string name, int id, LibrarySystem* set_library)
 void Librarian::AddBook(Book* newBook) { library->bookSystem->AddContent(newBook); }
 
 void Librarian::RemoveBook(int ISBN) { library->bookSystem->RemoveContent(ISBN); }
+
+void Librarian::SetLibrary(LibrarySystem* newLibrary) { library = &newLibrary; }
