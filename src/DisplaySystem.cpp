3b60ebae8cf2c85df5b6a80597152c9fa9d258f2
jsongi <unavailablenamenow@gmail.com>
2021-11-22 14:45:17 -0800

    Beginning implementations for displaying sorts / searches

--- src/DisplaySystem.cpp
+++ src/DisplaySystem.cpp
@@ -11,11 +11,45 @@ void DisplaySystem::DisplayBooks(std::vector<Book*> catalogue)
 	}
 }
 
-void DisplaySystem::DisplaySystem(std::vector<CheckedOut*> checkOut)
+void DisplaySystem::DisplayBooks(std::vector<CheckedOut*> checkOut)
 {
 	for(CheckedOut c : checkOut)
 	{
 		c.Display();
 		std::cout << endl;
+	}
+}
+void DisplaySystem::DisplayBooks(char choice, std::vector<Book*> catalogue)
+{
+	//Choice taken from main via user input from a menu of choices
+	std::vector<Book*> temp;
+	std::string genre;
+	int ISBN;
+	switch(choice {
+	//Case for GenreSearch
+	case '1':
+		searchAlgorithm = new GenreSearch();
+		temp = searchAlgorithm->SearchBooks(catalogue);
+		this->DisplayBooks(temp);
+		break;
+	//Case for SubgenreSearch
+	case '2':
+		searchAlgorithm = new SubgenreSearch();
+		temp = searchAlgorith->SearchBooks(catalogue);
+		this->DisplayBooks(temp);
+		break;
+	//Case for ISBNSearch
+	case '3':
+		searchAlgorithm = new ISBNSearch();
+		ISBN = 
+		temp = searchAlgorithm->SearchBooks(catalogue, ISBN);
+		this->DisplayBooks(temp);
+		break;
+	//Case for AlphabeticalSort
+	case '4':
+		sortAlgorithm = new AlphabeticalSort();
+		temp = sortAlgorithm->SortBooks(catalogue);
+		this->DisplayBooks(temp);
+		break;
 	}
 }
