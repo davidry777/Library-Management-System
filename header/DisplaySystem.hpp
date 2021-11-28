ccdc39e624c56805dac3547ea8d0309ba3681ec6
jsongi <unavailablenamenow@gmail.com>
2021-11-27 16:32:45 -0800

    Outlining display overloads for DisplaySystem

--- header/DisplaySystem.hpp
+++ header/DisplaySystem.hpp
@@ -13,10 +13,10 @@ private:
 public:
 	DisplaySystem();
 	~DisplaySystem();
-	void DisplayBooks(std::vector<Book*>);
+	void DisplayBooks(std::unordered_map<int, Content*>);
 	void DisplayBooks(std::vector<CheckedOut*>)
-	void DisplayBooks(char, std::string, std::vector<Book*>);
-	void DisplayBooks(char, int, std::vector<Book*>);
-	void DisplayBooks(char, std::vector<Book*>);
+	void DisplayBooks(char, std::string, std::unordered_map<int, Content*>>);
+	void DisplayBooks(char, int, std::unordered_map<int, Content*>>);
+	void DisplayBooks(char, std::unordered_map<int, Content*>>);
 };
 #endif
