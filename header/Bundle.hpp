47136fc87fce71e2069ee0ae229c017622559c22
daniel-u-c <dcast162@ucr.edu>
2021-11-29 14:39:29 -0800

    Updated Content's derived classes' logic

--- header/Bundle.hpp
+++ header/Bundle.hpp
@@ -9,10 +9,11 @@
 
 class Bundle : public Content {
     private:
-        std::unordered_map<int, Content*> contentList;
+        std::unordered_map<long long, Content*> contentList;
     public:
-        Bundle(const std::string& title, long long ISBN, const std::string& genre, const std::string& author, const std::vector<Content*>& contentList = {}, int frequency = 0);
+        Bundle(const std::string& title, long long ISBN, const std::string& genre, const std::vector<Content*>& contentList = {}, int frequency = 0);
         ~Bundle();
 
+        std::string GetAuthor();
         std::string Display(std::string indent = "");
 };
