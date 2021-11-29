191513e768c2ec1d7491a77cb03e65363e48e774
jsongi <unavailablenamenow@gmail.com>
2021-11-29 15:44:53 -0800

    Should be able to call ISBNSearch now

--- header/BookSearch.hpp
+++ header/BookSearch.hpp
@@ -9,6 +9,7 @@ class BookSearch {
 
  public:
 	virtual std::vector<Content*> SearchBooks(std::unordered_map<long long, Content*>, std::string);
+	virtual std::vector<Content*> SearchBooks(std::unordered_map<long  long, Content*>, int);
 	virtual std::vector<Content*> SearchBooks(std::vector<CheckOutData*> std::string);
 	std::vector<Content*> MapToVector(std::unordered_map<long long, Content*> catalogue)
 	{
