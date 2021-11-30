6e644a1e39b6095ad208fc6bf8fa7fa7f59af34a
jsongi <unavailablenamenow@gmail.com>
2021-11-30 10:15:32 -0800

    FrequencySort tests + header

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,11 @@
 #include "gtest/gtest.h"
 
 #include "BookTests.hpp"
-// #include "SortTests.hpp"
-// #include "PersonTests.hpp"
-// #include "BundleTests.hpp"
-// #include "BookSystemTests.hpp"
+#include "SortTests.hpp"
+#include "SearchTests.hpp"
+#include "PersonTests.hpp"
+#include "BundleTests.hpp"
+#include "BookSystemTests.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
