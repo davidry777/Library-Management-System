a08878e75cdfec02a898a585aeda324a593c7607
daniel-u-c <dcast162@ucr.edu>
2021-11-30 02:22:19 -0800

    Fixed bugs in files

--- header/BookSystem.hpp
+++ header/BookSystem.hpp
@@ -26,13 +26,14 @@ class BookSystem {
         std::string checkedOutFile; 
         std::string passedDueFile; 
 
-        void LoadCatalogue();
-        void LoadCheckedOut(UserSystem* us);
-        void LoadPassedDue(UserSystem* us);
     public:
         BookSystem(const std::string& catalogueFile, const std::string& checkedOutFile, const std::string& passedDueFile);
         ~BookSystem();
 
+        void LoadCatalogue();
+        void LoadCheckedOut(UserSystem* us);
+        void LoadPassedDue(UserSystem* us);
+        
         void SaveCatalogue(string file = "null");
         void SaveCheckedOut(string file = "null");
         void SavePassedDue(string file = "null");
