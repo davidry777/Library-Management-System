cc8fbb9cf6ec625d53df818d4adbcef8e7d35074
jsongi <unavailablenamenow@gmail.com>
2021-11-30 13:25:51 -0800

    compilation fix

--- header/BookSearch.hpp
+++ header/BookSearch.hpp
@@ -10,9 +10,9 @@ class BookSearch {
  private:
 
  public:
-	virtual std::vector<Content*> SearchBooks(std::unordered_map<long long, Content*>, std::string);
-	virtual std::vector<Content*> SearchBooks(std::unordered_map<long  long, Content*>, int);
-	virtual std::vector<Content*> SearchBooks(std::vector<CheckOutData*>, std::string);
+	std::vector<Content*> SearchBooks(std::unordered_map<long long, Content*>, std::string);
+	std::vector<Content*> SearchBooks(std::unordered_map<long  long, Content*>, int);
+	std::vector<Content*> SearchBooks(std::vector<CheckOutData*>, std::string);
 	std::vector<Content*> MapToVector(std::unordered_map<long long, Content*> catalogue)
 	{
 		std::vector<Content*> mapped;
