0fe89009f400c6725a5f4611efcad352da498c31
davidry777 <dryan0319@gmail.com>
2021-11-30 17:30:52 -0800

    Resolved all memory leaks in PersonTests.hpp

--- tests/PersonTests.hpp
+++ tests/PersonTests.hpp
@@ -12,11 +12,6 @@
 #include <vector>
 #include <string>
 
-BookSystem* genBookSys()
-{
-    return new BookSystem("../tests/test_catalogue.json", "none", "none");
-}
-
 TEST(PersonTest, GetName)
 {
     User user("Jason Giese", 28956, nullptr, 830264);
@@ -108,7 +103,7 @@ TEST(LibrarianTest, AddingBook)
 TEST(LibrarianTest, RemovingBook)
 {
     BookSystem* setBookSys = new BookSystem("../tests/test_catalogue.json", "none", "none");
-    Librarian librarian("Gwen Kiler", 78094, genBookSys(), 403501);
+    Librarian librarian("Gwen Kiler", 78094, setBookSys, 403501);
     Book* book = new Book("How to be a CS God", 9784567290812, "Non Fiction", "David Ryan", 0);
     librarian.AddBook(book);
     librarian.RemoveBook(9784567290812);
