0502d774ec5d0394890f9f83a62662bea2c7c6a0
daniel-u-c <dcast162@ucr.edu>
2021-11-30 22:19:48 -0800

    LibrarySystem tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,12 @@
 #include "gtest/gtest.h"
 
 #include "BookTests.hpp"
-#include "SortTests.hpp"
-#include "SearchTests.hpp"
-// #include "PersonTests.hpp"
 #include "BundleTests.hpp"
 #include "BookSystemTests.hpp"
+#include "LibrarySystemTests.hpp"
+#include "SortTests.hpp"
+#include "SearchTests.hpp"
+#include "PersonTests.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
