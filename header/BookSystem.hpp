b175aa719d1551a39a135b1d803a65de51bd171e
daniel-u-c <dcast162@ucr.edu>
2021-12-01 00:03:13 -0800

    Reworked the check-out system

--- header/BookSystem.hpp
+++ header/BookSystem.hpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include <unordered_map>
-#include <deque>
 #include <ctime>
 #include <iomanip>
 #include <fstream>
@@ -17,6 +16,7 @@
 #include "Person.hpp"
 
 #include <vector>
+#include <set>
 #include "CheckOutData.hpp"
 
 class UserSystem;
@@ -25,30 +25,26 @@ class CheckOutData;
 class BookSystem {
     private:
         std::unordered_map<long long, Content*> catalogue;
-        std::vector<CheckOutData*> passedDue;
-        std::deque<CheckOutData*> checkedOut;
+        std::unordered_map<int, std::set<CheckOutData*>> checkedOut;
 
         std::string catalogueFile; 
-        std::string checkedOutFile; 
-        std::string passedDueFile; 
+        std::string checkedOutFile;
 
+        int maximumSeconds;
     public:
-        BookSystem(const std::string& catalogueFile, const std::string& checkedOutFile, const std::string& passedDueFile);
+        BookSystem(const std::string& catF, const std::string& coF, int maxSec = 259200);
         ~BookSystem();
 
         void LoadCatalogue();
-        void LoadCheckedOut(std::unordered_map<int, Person *>);
-        void LoadPassedDue(std::unordered_map<int, Person *>);
-        
         void SaveCatalogue(std::string file = "null");
-        void SaveCheckedOut(std::string file = "null");
-        void SavePassedDue(std::string file = "null");
-
-        Content* GetContent(long long ISBN);
-        std::vector<CheckOutData*>& GetPassedDue();
-        std::deque<CheckOutData*>& GetCheckedOut();
         std::unordered_map<long long, Content*>& GetCatalogue();
 
+        void LoadCheckedOut(std::unordered_map<int, Person *>);
+        void SaveCheckedOut(std::string file = "null");
+        unordered_map<int, std::set<CheckOutData*>>& GetCheckedOut();
+
+
+        Content* GetContent(long long ISBN);
         bool AddContent(Content* content);
         bool RemoveContent(long long ISBN);
         bool MakeBundle(const std::string& title, long long ISBN, const std::string& genre, const std::vector<Content*>& contents);
