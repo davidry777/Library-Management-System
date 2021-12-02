6f1076ac8572c8e825bf92557eae9c21b2173666
gkiler <gwenkiler@gmail.com>
2021-12-02 00:05:27 +0000

    various testing fixes usersystem and loginsystem

--- header/LoginSystem.hpp
+++ header/LoginSystem.hpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <fstream>
 #include <string>
+#include "Person.hpp"
 
 using namespace std;
 
@@ -10,10 +11,10 @@ class LoginSystem {
 	private:
 		unordered_map<int, int> userPasswords; //<userID, hashed password>
 	public:
-		LoginSystem(std::string textFile);
+    LoginSystem(unordered_map<int, Person*> userList);
 		bool LoginVerify(int userID, std::string userPass);
 		int HashPassword(std::string userPass);
-		void SaveUsers(std::string textFile);
+		// void SaveUsers(std::string textFile);
 		int GetHashPass(int ID);
     void AddPass(int ID, int userPass);
 };
