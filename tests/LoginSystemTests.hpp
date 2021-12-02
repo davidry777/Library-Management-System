690a9f72fbd4efb565d66ba8aa46c6953737fe44
gkiler <73858764+gkiler@users.noreply.github.com>
2021-12-01 16:47:36 -0800

    Update LoginSystemTests.hpp

--- tests/LoginSystemTests.hpp
+++ tests/LoginSystemTests.hpp
@@ -5,26 +5,26 @@
 #include "../header/LoginSystem.hpp"
 #include "../header/UserSystem.hpp"
 TEST(LoginSystemTest, Construct) {
-  UserSystem *userSys = new UserSystem("../tests/test_users.json", nullptr);
+  UserSystem *userSys = new UserSystem("../tests/test_files/test_users.json", nullptr);
   LoginSystem *logSys = new LoginSystem(userSys->GetMap());
   EXPECT_EQ(logSys->GetHashPass(18658864), 11464202);
 }
 TEST(LoginSystemTest, LoginVerify) {
-  UserSystem *userSys = new UserSystem("../tests/test_users.json", nullptr);
+  UserSystem *userSys = new UserSystem("../tests/test_files/test_users.json", nullptr);
   LoginSystem *logSys = new LoginSystem(userSys->GetMap());
   int ID = 18658864;
   string pass = "Applesauce";
   EXPECT_TRUE(logSys->LoginVerify(ID, pass));
 }
 TEST(LoginSystemTest, LoginVerifyFailure) {
-  UserSystem *userSys = new UserSystem("../tests/test_users.json", nullptr);
+  UserSystem *userSys = new UserSystem("../tests/test_files/test_users.json", nullptr);
   LoginSystem *logSys = new LoginSystem(userSys->GetMap());
   int ID = 18658864;
   string pass = "wrong password";
   EXPECT_FALSE(logSys->LoginVerify(ID, pass));
 }
 TEST(LoginSystemTest, SavedHash) {
-  UserSystem *userSys = new UserSystem("../tests/test_users.json", nullptr);
+  UserSystem *userSys = new UserSystem("../tests/test_files/test_users.json", nullptr);
   LoginSystem *logSys = new LoginSystem(userSys->GetMap());  
   int ID = 34;
   int pass = 89000;
@@ -32,14 +32,14 @@ TEST(LoginSystemTest, SavedHash) {
   EXPECT_EQ(logSys->GetHashPass(34), 89000);
 }
 TEST(LoginSystemTest, EmptyHash) {
-  UserSystem *userSys = new UserSystem("../tests/test_users.json", nullptr);
+  UserSystem *userSys = new UserSystem("../tests/test_files/test_users.json", nullptr);
   LoginSystem *logSys = new LoginSystem(userSys->GetMap());  int ID = 34;  
   string pass = ""; //39052307
   EXPECT_EQ(logSys->HashPassword(pass),0);
 }
 
 TEST(LoginSystemTest, GetHashPass) {
-  UserSystem *userSys = new UserSystem("../tests/test_users.json", nullptr);
+  UserSystem *userSys = new UserSystem("../tests/test_files/test_users.json", nullptr);
   LoginSystem *logSys = new LoginSystem(userSys->GetMap());  int ID = 34;  
   string pass = "Lskdjf;lksjdgkjdsdg"; //39052307
   EXPECT_EQ(logSys->HashPassword(pass),39052307);
